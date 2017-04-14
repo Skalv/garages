e7c59cff840d7e1cfd5848eb4af05a61f3a76f2e
Plysken Rayder <jey_de_ressac@msn.Com>
2017-04-14 22:17:46 +0200

    Garages
    Garages is a FiveM Module to store or recover your vehicle.

---
+++ README.md
@@ -0,0 +1,23 @@
+# garages v0.1.0
+
+garages is a FiveM Module to store or recover your vehicle.
+
+ **Note:
+
+## Changelog
+You can find our changelog [here](CHANGELOG.md)
+
+## Requirements
+- [Essentialmode](https://github.com/FiveM-Scripts/essentialmode)
+- [es_freeroam](https://github.com/FiveM-Scripts/es_freeroam)
+
+## Installation
+1. Download
+2. Extract the folder
+3. Place the folder in your resources/[folder]
+4. Add - garage to your AutoStartResources in citmp-server.yml
+5. Open **server.lua** and change your database settings.
+6. Restart your server
+
+## Upgrade
+- 
---
+++ server.lua
@@ -0,0 +1,31 @@
+require "resources/essentialmode/lib/MySQL"
+MySQL:open("localhost", "GTA5", "GTA5", "rb1st8mm")
+
+RegisterServerEvent('garages:CheckForVeh')
+AddEventHandler('garages:CheckForVeh', function(vehicle)
+  --local joueur = "1"
+  --print("Player ID " ..source)
+	--TriggerEvent('es:getPlayerFromId', source, function(user)
+
+  --print(source)
+  --print(user)
+
+  local player = source
+  local executed_query = MySQL:executeQuery("SELECT personalvehicle FROM users WHERE id = '@username'",{['@username'] = source})
+  local result = MySQL:getResults(executed_query, {'personalvehicle'})
+
+        if(result)then
+    for k,v in ipairs(result)do
+      print(v.personalvehicle)
+      vehicle = v.personalvehicle
+    end
+    end
+  local vehicle = vehicle
+  TriggerClientEvent('garages:SpawnVehicle', source, vehicle)
+
+end)
+
+RegisterServerEvent('garages:GetForVeh')
+AddEventHandler('garages:GetForVeh', function(vehicle)
+
+end)
