6762d733c2e1ceff5ce360503634c93bf9f0968c
plysken <jey_de_ressac@msn.Com>
2017-04-15 18:11:39 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,31 +1,25 @@
 require "resources/essentialmode/lib/MySQL"
-MySQL:open("localhost", "GTA5", "GTA5", "pass")
+MySQL:open("localhost", "GTA5", "GTA5", "password")
 
 RegisterServerEvent('garages:CheckForVeh')
-AddEventHandler('garages:CheckForVeh', function(vehicle)
-  --local joueur = "1"
-  --print("Player ID " ..source)
-	--TriggerEvent('es:getPlayerFromId', source, function(user)
-
-  --print(source)
-  --print(user)
-
-  local player = source
-  local executed_query = MySQL:executeQuery("SELECT personalvehicle FROM users WHERE id = '@username'",{['@username'] = source})
-  local result = MySQL:getResults(executed_query, {'personalvehicle'})
-
-        if(result)then
-    for k,v in ipairs(result)do
-      print(v.personalvehicle)
-      vehicle = v.personalvehicle
+AddEventHandler('garages:CheckForVeh', function()
+  print(source)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local executed_query = MySQL:executeQuery("SELECT personalvehicle FROM users WHERE identifier = '@username'",{['@username'] = player})
+    local result = MySQL:getResults(executed_query, {'personalvehicle'}, "identifier")
+    if(result)then
+      for k,v in ipairs(result)do
+        print(v.personalvehicle)
+        vehicle = v.personalvehicle
+      end
     end
-    end
-  local vehicle = vehicle
-  TriggerClientEvent('garages:SpawnVehicle', source, vehicle)
-
+    TriggerClientEvent('garages:SpawnVehicle', source, vehicle)
+  end)
 end)
 
 RegisterServerEvent('garages:GetForVeh')
-AddEventHandler('garages:GetForVeh', function(vehicle)
 
+AddEventHandler('garages:GetForVeh', function(vehicle)
+  --to vérifier le si le véhicule a mettre dans le garage est celui qui est possédé
 end)
