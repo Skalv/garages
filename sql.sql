753eae7887dc55c33759b3c89a1f79846ddb78a2
plysken <jey_de_ressac@msn.Com>
2017-04-16 20:17:12 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `user_vehicle` (
   `identifier` varchar(255) NOT NULL,
   `vehicle_model` varchar(60) DEFAULT NULL,
   `vehicle_plate` varchar(60) DEFAULT NULL,
-  `vehicle_state` varchar(60) DEFAULT NULL
+  `vehicle_state` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
