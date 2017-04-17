1c48126538470040e32d7fc2109b6660971e9503
plysken <jey_de_ressac@msn.Com>
2017-04-17 21:18:36 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,26 +5,54 @@ RegisterServerEvent('garages:CheckForSpawnVeh')
 RegisterServerEvent('garages:CheckForVeh')
 RegisterServerEvent('garages:SetVehOut')
 RegisterServerEvent('garages:SetVehIn')
+RegisterServerEvent('garages:PutVehInGarages')
 
+AddEventHandler('garages:PutVehInGarages', function(vehicle)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+
+    local player = user.identifier
+    local state ="In"
+
+    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username'",{['@username'] = player})
+    local result = MySQL:getResults(executed_query, {'identifier'})
+
+    if(result)then
+
+      --local executed_query = MySQL:executeQuery("INSERT INTO user_vehicle (`identifier`, `vehicle_model`, `vehicle_plate`, `vehicle_state`, `vehicle_colorprimary`, `vehicle_colorsecondary`) VALUES ('@username', '@vehicle', '@plate', '@state', '@primaryColor', '@secondaryColor')",
+      --{['@username'] = player, ['@vehicle'] = vehicle, ['@plate'] = plate, ['@state'] = state, ['@primaryColor'] = primaryColor, ['@secondaryColor'] = secondaryColor})
+
+    --else
+
+      local executed_query = MySQL:executeQuery("UPDATE user_vehicle SET `vehicle_state`='@state' WHERE identifier = '@username'",
+      {['@username'] = player, ['@state'] = state})
+
+    end
+  end)
+end)
 
 AddEventHandler('garages:CheckForSpawnVeh', function()
   --print(source)
   TriggerEvent('es:getPlayerFromId', source, function(user)
     local player = user.identifier
     local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username'",{['@username'] = player})
-    local result = MySQL:getResults(executed_query, {'vehicle_model', 'vehicle_plate', 'vehicle_state'}, "identifier")
+    local result = MySQL:getResults(executed_query, {'vehicle_model', 'vehicle_plate', 'vehicle_state', 'vehicle_colorprimary', 'vehicle_colorsecondary'}, "identifier")
     if(result)then
       for k,v in ipairs(result)do
         --print(v.personalvehicle)
         vehicle = v.vehicle_model
         plate = v.vehicle_plate
         state = v.vehicle_state
+        primarycolor = v.vehicle_colorprimary
+        secondarycolor = v.vehicle_colorsecondary
+
       local vehicle = vehicle
       local plate = plate
-      local state = state
+      local state = state      
+      local primarycolor = primarycolor
+      local secondarycolor = secondarycolor
       end
     end
-    TriggerClientEvent('garages:SpawnVehicle', source, vehicle, plate, state)
+    TriggerClientEvent('garages:SpawnVehicle', source, vehicle, plate, state, primarycolor, secondarycolor)
   end)
 end)
 
